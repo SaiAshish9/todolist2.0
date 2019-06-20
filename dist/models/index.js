9a19a76a88d12399b07dc17361b58ba753b269ad
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-06-20 18:49:10 +0530

    Update index.js

--- dist/models/index.js
+++ dist/models/index.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 mongoose.set('debug',true);
-mongoose.connect("mongodb+srv://Sai_99:shirdisai@cluster0-4bk2v.mongodb.net/todolist2DB"
+mongoose.connect("todolist2DB"
   // "mongodb://localhost:27017/todolist2"
 ,{useNewUrlParser:true})
 
